a06463f7a1649100353570d7a673ebca2b9955c8
romatimon <119756808+romatimon@users.noreply.github.com>
2023-08-25 16:08:05 +0300

    Add files via upload

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -2,12 +2,15 @@ import pytest
 from django.conf import settings
 from django.urls import reverse
 
+from news.forms import CommentForm
+
+HOME_URL = reverse('news:home')
+
 
 @pytest.mark.django_db
 def test_news_count(client, all_news):
     """Количество новостей на главной странице — не более 10."""
-    url = reverse('news:home')
-    response = client.get(url)
+    response = client.get(HOME_URL)
     object_list = response.context['object_list']
     news_count = len(object_list)
     assert news_count == settings.NEWS_COUNT_ON_HOME_PAGE
@@ -16,9 +19,9 @@ def test_news_count(client, all_news):
 @pytest.mark.django_db
 def test_news_order(client, all_news):
     """Новости отсортированы от самой свежей к самой старой.
-    Свежие новости в начале списка."""
-    url = reverse('news:home')
-    response = client.get(url)
+    Свежие новости в начале списка.
+    """
+    response = client.get(HOME_URL)
     object_list = response.context['object_list']
     all_dates = [news.date for news in object_list]
     sorted_dates = sorted(all_dates, reverse=True)
@@ -29,7 +32,8 @@ def test_news_order(client, all_news):
 def test_comments_order(client, news, slug_for_comment):
     """Комментарии на странице отдельной новости отсортированы
     в хронологическом порядке: старые в начале списка,
-    новые — в конце."""
+    новые — в конце.
+    """
     detail_url = reverse('news:detail', args=slug_for_comment)
     response = client.get(detail_url)
     news = response.context['news']
@@ -38,18 +42,24 @@ def test_comments_order(client, news, slug_for_comment):
 
 
 @pytest.mark.django_db
-def test_authorized_client_has_form(author_client, slug_for_args):
+def test_authorized_client_has_form(author_client,
+                                    news_detail_url):
     """Авторизированному пользователю доступна форма для отправки
-    комментария на странице отдельной новости"""
-    detail_url = reverse('news:detail', args=slug_for_args)
-    response = author_client.get(detail_url)
+    комментария на странице отдельной новости
+    """
+    response = author_client.get(news_detail_url)
     assert 'form' in response.context
+    form = response.context.get('form')
+    assert isinstance(form, CommentForm)
+
 
 
 @pytest.mark.django_db
-def test_anonymous_client_has_no_form(client, slug_for_args):
+def test_anonymous_client_has_no_form(client,
+                                      news_detail_url):
     """Неавторизованному пользователю недоступна форма для отправки
-    комментария на странице отдельной новости"""
-    detail_url = reverse('news:detail', args=slug_for_args)
-    response = client.get(detail_url)
+    комментария на странице отдельной новости
+    """
+    response = client.get(news_detail_url)
     assert 'form' not in response.context
+
--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -7,31 +7,40 @@ from django.urls import reverse
 from news.models import Comment
 from news.forms import WARNING
 
+LOGIN_URL = reverse('users:login')
+
 
 @pytest.mark.django_db
-def test_anonymous_user_cant_create_comment(client, form_data, slug_for_args):
+def test_anonymous_user_cant_create_comment(client, form_data,
+                                            news_detail_url):
     """Анонимный пользователь не может отправить комментарий."""
-    detail_url = reverse('news:detail', args=slug_for_args)
-    response = client.post(detail_url, data=form_data)
-    login_url = reverse('users:login')
-    expected_url = f'{login_url}?next={detail_url}'
+    comment_count = Comment.objects.count()
+    assert comment_count == 0
+    response = client.post(news_detail_url, data=form_data)
+    expected_url = f'{LOGIN_URL}?next={news_detail_url}'
     assertRedirects(response, expected_url)
     assert Comment.objects.count() == 0
 
 
-def test_user_cant_create_comment(author_client, slug_for_args, form_data):
+def test_user_cant_create_comment(author_client, news_detail_url, form_data):
     """Авторизованный пользователь может отправить комментарий."""
-    detail_url = reverse('news:detail', args=slug_for_args)
-    response = author_client.post(detail_url, data=form_data)
-    assertRedirects(response, f'{detail_url}#comments')
+    response = author_client.post(news_detail_url, data=form_data)
+    assertRedirects(response, f'{news_detail_url}#comments')
     assert Comment.objects.count() == 1
+    new_comment = Comment.objects.get()
+    assert new_comment.text == form_data['text']
 
 
-def test_user_cant_use_bad_words(author_client, bad_words_data, slug_for_args):
+def test_user_cant_use_bad_words(author_client,
+                                 bad_words_data,
+                                 news_detail_url):
     """Если комментарий содержит запрещённые слова, он не будет опубликован,
-    а форма вернёт ошибку."""
-    detail_url = reverse('news:detail', args=slug_for_args)
-    response = author_client.post(detail_url, data=bad_words_data)
+    а форма вернёт ошибку.
+    """
+    comment_bad_words = Comment.objects.count()
+    assert comment_bad_words == 0
+    response = author_client.post(news_detail_url,
+                                  data=bad_words_data)
     assertFormError(
         response,
         form='form',
@@ -41,38 +50,49 @@ def test_user_cant_use_bad_words(author_client, bad_words_data, slug_for_args):
     assert Comment.objects.count() == 0
 
 
-def test_author_can_edit_comment(author_client, comment, form_data,
-                                 news_edit_url, slug_for_args):
+def test_author_can_edit_comment(author_client,
+                                 comment, form_data,
+                                 news_edit_url,
+                                 news_detail_url):
     """Авторизованный пользователь может редактировать свои комментарии."""
-    url = reverse('news:detail', args=slug_for_args)
     response = author_client.post(news_edit_url, form_data)
-    assertRedirects(response, f'{url}#comments')
+    assertRedirects(response, f'{news_detail_url}#comments')
     comment.refresh_from_db()
     assert comment.text == form_data['text']
+    assert comment.author == comment.author
+    assert comment.news == comment.news
 
 
 @pytest.mark.django_db
-def test_user_cant_edit_comment_of_another_user(admin_client, news_edit_url,
-                                                form_data, comment):
+def test_user_cant_edit_comment_of_another_user(admin_client,
+                                                news_edit_url,
+                                                form_data,
+                                                comment):
     """Авторизованный пользователь не может редактировать чужие комментарии."""
     response = admin_client.post(news_edit_url, form_data)
     assert response.status_code == HTTPStatus.NOT_FOUND
     comment_from_db = Comment.objects.get(id=comment.id)
     assert comment.text == comment_from_db.text
+    assert comment.author == comment.author
+    assert comment.news == comment.news
 
 
-def test_author_can_detele_comment(author_client, comment, news_delete_url,
-                                   slug_for_args):
+def test_author_can_detele_comment(author_client,
+                                   comment,
+                                   news_delete_url,
+                                   news_detail_url):
     """Авторизованный пользователь может удалять свои комментарии."""
-    url = reverse('news:detail', args=slug_for_args)
+    comments_count = Comment.objects.count()
+    assert comments_count == 1
     response = author_client.post(news_delete_url)
-    assertRedirects(response, f'{url}#comments')
+    assertRedirects(response, f'{news_detail_url}#comments')
     assert Comment.objects.count() == 0
 
 
 @pytest.mark.django_db
 def test_user_cant_delete_comment_of_another_user(admin_client,
-                                                  news_delete_url, form_data,
+                                                  news_delete_url,
+                                                  form_data,
                                                   comment):
     """Авторизованный пользователь не может удалять чужие комментарии."""
     response = admin_client.post(news_delete_url, form_data)
